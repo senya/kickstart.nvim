a838ed703726322a2b2786d11032fc9cc97cab50
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2025-11-26 00:41:16 +0300

    ensure_installed
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>

--- init.lua
+++ init.lua
@@ -899,12 +899,23 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'clang-format', -- Used to format C/C++ code
+        'clang-format',
+        'ktlint',
+        'ktfmt',
+        'markdownlint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        ensure_installed = {
+          'clangd',
+          'pyright',
+          'kotlin_lsp',
+          'ltex',
+          'ltex_plus',
+          'lua_ls',
+          'stylua',
+        },
         handlers = {
           function(server_name)
             -- Пропускаем ltex
