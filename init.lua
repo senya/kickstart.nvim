582603a05165cdf29d324f80330c7efc284e89eb
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2025-11-25 21:45:50 +0300

    Переходим на kotlin-lsp официальный!

--- init.lua
+++ init.lua
@@ -859,7 +859,7 @@ require('lazy').setup({
         },
         -- gopls = {},
         pyright = {},
-        kotlin_language_server = { cmd = { 'cgexec', '-g', 'memory,cpu:javagroup', 'kotlin-language-server' } },
+        kotlin_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
